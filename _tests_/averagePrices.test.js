4a18d981217209eb354c1343cd9e368c92cb8898
benizza haytam <haytam.benizza@avaliance.com>
2023-03-23 14:32:18 +0000

    add test feature 1

---
+++ _tests_/averagePrices.test.js
@@ -0,0 +1,44 @@
+import { handler } from "../pages/api/average-prices";
+import { getMonthlyAverages } from "../helpers/average-prices";
+import googleStockPrice from "../public/data/google-stock-price.json";
+import amazoneStockPrice from "../public/data/amazone-stock-price.json";
+
+// Mock the response object for testing
+const createResponse = () => {
+  const res = {
+    status: jest.fn(() => res),
+    json: jest.fn(() => res),
+  };
+  return res;
+};
+
+describe("averagePrices API endpoint", () => {
+  it("returns 405 status code for non GET requests", async () => {
+    const req = { method: "POST" };
+    const res = createResponse();
+
+    await handler(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(405);
+    expect(res.json).toHaveBeenCalledWith({ message: "Method not allowed" });
+  });
+
+  it("returns monthly average prices", async () => {
+    const req = { method: "GET" };
+    const res = createResponse();
+
+    await handler(req, res);
+
+    const expectedGoogleStockPrice = [...getMonthlyAverages(googleStockPrice)];
+
+    const expectedAmazoneStockPrice = [
+      ...getMonthlyAverages(amazoneStockPrice),
+    ];
+
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith({
+      googleStockPrices: expectedGoogleStockPrice,
+      amazonStockPrices: expectedAmazoneStockPrice,
+    });
+  });
+});
